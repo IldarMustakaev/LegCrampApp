a6fd0f87ab1ad83c019d2b28b28ba768326dd931
i.mustakaev@qiwi.ru <i.mustakaev@qiwi.ru>
2016-01-30 00:08:17 +0300

    Initial Commit

---
+++ LegCrampApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LegCrampApp
+//
+//  Created by i.mustakaev on 1/30/16.
+//  Copyright © 2016 i.mustakaev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LegCrampApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LegCrampApp
+//
+//  Created by i.mustakaev on 1/30/16.
+//  Copyright © 2016 i.mustakaev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
